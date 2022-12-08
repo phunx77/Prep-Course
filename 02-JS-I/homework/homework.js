fd752e3d64b8ec9002fd0b2e6d5070124f814e0b
cristianflores88_22@hotmail.com <cristianflores88_22@hotmail.com>
2022-12-07 21:19:41 -0300

    Tarea js II

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -210,6 +210,8 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  // let stringConcatenadas = nombre + ' ' + apellido
+  // return stringConcatenadas = nombre
   return nombre + " " + apellido;
   
 }
@@ -218,6 +220,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  // let nuevaString = 'Hola ' + nombre + '!'; 
+  // return nuevaString;
   return 'Hola ' + nombre + "!";
   
 }
@@ -225,6 +229,7 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  // let area = ancho * alto;
   return alto * ancho;
 }
 
